aa04cf102b23826992d5fdbab6580f6c6f98ab0e
Devpareek8 <165389457+Devpareek8@users.noreply.github.com>
2025-04-02 07:06:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,16 +14,16 @@ def is_enabled(value, default):
         return default
 
 
-ADMIN = int(getenv("ADMIN", ""))
+ADMIN = int(getenv("ADMIN", "6914788581"))
 SILICON_PIC = os.environ.get("SILICON_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
-API_ID = int(getenv("API_ID", ""))
-API_HASH = str(getenv("API_HASH", ""))
+API_ID = int(getenv("API_ID", "24942826"))
+API_HASH = str(getenv("API_HASH", "e3e2f3b65ef58634139ccd27d6b7d8cb"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
 FORCE_SUB = os.environ.get("FORCE_SUB", "") 
-MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://replacewithyourmongodb:replacewithyourmongodb@cluster0.zi78j51.mongodb.net/?retryWrites=true&w=majority",))
+MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://Demo23:Demo23@cluster0.fjar36c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
 DEF_CAP = str(
     getenv(
         "DEF_CAP",
-        "<b>File Name:- `{file_name}`\n\n{file_size}</b>",
+        "<b>File Name:- `{file_name}`\n\n{file_size}\Dev</b>",
     )
 )
